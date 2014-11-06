f7258072dbe39de4b449672ba854b854f07a541c
Ben Ramseth <bramseth@esri.com>
2014-11-06 10:42:14 -0500

    Added two layers and dijit button

--- js/map.js
+++ js/map.js
@@ -1,3 +1,28 @@
 /**
  * Created by ben3029 on 11/6/14.
  */
+
+var map;
+
+require(["esri/map", "esri/layers/ArcGISDynamicMapServiceLayer",
+    "dijit/form/Button",
+    "dojo/domReady!"], function(Map,ArcGISDynamicMapServiceLayer,Button) {
+    map = new Map("map", {
+        basemap: "satellite",
+        center: [-76.030, 36.745], // longitude, latitude
+        zoom: 11
+    });
+
+    var parcellayer = new ArcGISDynamicMapServiceLayer("https://gismaps.vbgov.com/arcgis/rest/services/Basemaps/PropertyInformation_VBgov/MapServer", {
+        "id": "parcellayer",
+        "opacity": 1
+    });
+    map.addLayer(parcellayer);
+
+    var footPrintLayer = new ArcGISDynamicMapServiceLayer("https://gismaps.vbgov.com/arcgis/rest/services/Basemaps/Structures_and_Physical_Features/MapServer", {
+        "id": "footPrintLayer",
+        "opacity": 1
+    });
+    map.addLayer(footPrintLayer);
+
+});
